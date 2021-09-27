dd8380f881f88378ba4c5bc7ab5c2676f0254cff
Rio-Sh <shrio.wr@gmail.com>
2021-09-27 23:33:37 +0900

    Add Navigation Drawer

--- app/src/main/java/com/example/studymytaskapp/MainActivity.kt
+++ app/src/main/java/com/example/studymytaskapp/MainActivity.kt
@@ -2,12 +2,49 @@ package com.example.studymytaskapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.studymytaskapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupNavigation()
+    }
+
+    /**
+     * Called when hamburger button is tapped or back button is pressed
+     *
+     * Delegate to Navigation.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        //val navController = findNavController(R.id.nav_host_fragment)
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!.findNavController()
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
+    }
+
+    /**
+     * Setup Navigation for Activity
+     */
+    private fun setupNavigation() {
+        setSupportActionBar(binding.toolbar)
+
+        //val navController = findNavController(R.id.nav_host_fragment)
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!.findNavController()
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
