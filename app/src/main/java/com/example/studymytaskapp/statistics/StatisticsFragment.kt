ce248a884c09f7899ce06f509b8bf3043b6d39a5
Rio-Sh <shrio.wr@gmail.com>
2021-10-25 21:42:04 +0900

    Implement Statistics

--- app/src/main/java/com/example/studymytaskapp/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/studymytaskapp/statistics/StatisticsFragment.kt
@@ -6,23 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.example.studymytaskapp.R
+import com.example.studymytaskapp.databinding.StatisticsFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = StatisticsFragment()
-    }
 
     private val viewModel: StatisticsViewModel by viewModels()
+    private lateinit var binding: StatisticsFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.statistics_fragment, container, false)
+
+        binding = StatisticsFragmentBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        return binding.root
     }
 
 }
